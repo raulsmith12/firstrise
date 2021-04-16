851fd60b2bae12958dc6e821ec567ace343af9c2
Raul Smith <raul.n.smith@gmail.com>
2021-04-15 18:52:20 -0600

    Updated header to include logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,13 @@ import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <Navbar fixed="top" bg="dark" variant="dark">
-            <Navbar.Brand href="/">First Rise Baking Co.</Navbar.Brand>
+        <Navbar fixed="top" bg="white" variant="light">
+            <Navbar.Brand href="/">
+                <img src="https://galacticblue.net/firstrisebakery/wp-content/uploads/2021/04/logo.jpg" alt="First Rise Bakery" className="header-logo" />
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="navigation-navbar" />
             <Navbar.Collapse id="navigation-navbar">
                 <Nav className="ml-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="/order">Order/Menu</Nav.Link>
                     <Nav.Link href="/profile">Profile</Nav.Link>
                 </Nav>
